4e1d53d0526f036596de976055f9b9408f9d1c78
ibrahimsiddik007 <101477351+ibrahimsiddik007@users.noreply.github.com>
2022-03-13 10:29:34 +0600

    Print the numbers in reverse.

---
+++ Exercise Problem -1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+
+    int n,i;
+    printf("Enter the max digit: ");
+    scanf("%d",&n);
+
+    for(i=n;i>=1;i--)
+        {
+        printf("%d\n",i);
+    }
+
+   return 0;
+}
